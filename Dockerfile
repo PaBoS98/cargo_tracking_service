20082a252d54fd186b02ea5d00977c1ea8b64233
p.bohomaz <p.bohomaz@andersenlab.com>
2024-04-04 14:03:56 +0300

    fix issue with docker port

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,8 @@ WORKDIR /app/cargo_tracking_service
 RUN mvn clean install
 
 FROM openjdk:17-jdk-slim
+ARG PORT=8080
+ENV PORT=${PORT}
 WORKDIR /app/cargo_tracking_service
 COPY --from=build /app/cargo_tracking_service/target/*.jar app.jar
-ENTRYPOINT ["java","-jar","app.jar"]
+ENTRYPOINT ["java", "-jar", "app.jar", "--server.port=${PORT}"]
--- src/main/java/com/pavlob/CargoTrackingServiceApplication.java
+++ src/main/java/com/pavlob/CargoTrackingServiceApplication.java
@@ -1,7 +1,6 @@
 package com.pavlob;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -11,12 +10,8 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @Slf4j
 public class CargoTrackingServiceApplication {
 
-    @Value("${server.port}")
-    private static String s;
-
     public static void main(String[] args) {
         SpringApplication.run(CargoTrackingServiceApplication.class, args);
-        log.info("pavlo test {}", s);
     }
 
 }
